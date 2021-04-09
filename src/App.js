b3a7394b7087d6ad5e8fba061f859793014baeaa
savannaelbey <savanna_e@icloud.com>
2021-04-09 11:11:04 +0100

    refactor

--- src/App.js
+++ src/App.js
@@ -10,20 +10,76 @@ function App() {
   const [outputChange, setOutputChange] = useState('');
 
   useEffect(() => {
+    function calculateCost() {
+      let timeInArr = timeIn.split(':');
+      let timeOutArr = timeOut.split(':');
+      let numOfMinsAtEntry = Number((timeInArr[0] * 60)) + Number(timeInArr[1])
+      let numOfMinsAtExit = Number((timeOutArr[0] * 60)) + Number(timeOutArr[1])
+      //if exit time is the following day, add 24 hours to timeout
+      if (Number(timeInArr[0]) <= Number(timeOutArr[0])) {
+        let numOfMinsInCarPark = numOfMinsAtExit - numOfMinsAtEntry;
+        if(numOfMinsInCarPark > 60) {
+          setOutputCost(3 + (numOfMinsInCarPark - 60) / 100);
+        } else if (numOfMinsInCarPark <= 60) {
+          setOutputCost(3);
+        }
+      } else {
+        let numOfMinsInCarPark = (numOfMinsAtExit + (24 * 60)) - numOfMinsAtEntry;
+        setOutputCost(3 + ((numOfMinsInCarPark - 60) / 100));
+      }
+    }
     calculateCost();
-  }, [calculateCost]);
+  }, [timeIn, timeOut]);
 
   useEffect(() => {
+    function processPaymentInput() {
+      let inputArray = paymentIn.split(', ');
+      let processedInputArray = [];
+      for (let element of inputArray) {
+        if (element[0] === '£') {
+          processedInputArray.push(Number(element.slice(1)));
+        } else {
+          processedInputArray.push(element.match(/\d+/g) / 100);
+        }
+      }
+      setPaymentArray(processedInputArray);
+    }
     processPaymentInput();
   }, [paymentIn]);
 
   useEffect(() => {
+    function calculateMinChange() {
+      //calculate sum of payment in
+      let paymentSum = paymentArray.reduce((a, b) => a + b, 0);
+      let change = (paymentSum - outputCost).toFixed(2)
+      let coins = [0.01, 0.02, 0.05, 0.10, 0.20, 0.50, 1, 5, 10, 20]
+      let descCoins = coins.sort((a,b) => b-a);
+      let changeArray = []
+      for (let i = 0; i < descCoins.length; i++) {
+        while (descCoins[i] <= change ) {
+          changeArray.push(descCoins[i]);
+          change = (change - descCoins[i]).toFixed(2)
+        }
+      }
+      setChangeArr(changeArray);
+    }
     calculateMinChange();
-  }, [paymentArray]);
+  }, [paymentArray, outputCost]);
 
   useEffect(() => {
+    function formatOutputChange() {
+      let formattedArray = []
+      for (let element of changeArr) {
+        if (element < 1) {
+          formattedArray.push(element * 100 + 'p')
+        } else {
+          formattedArray.push('£' + element)
+        }
+      }
+      setOutputChange(formattedArray.join(', '));
+    }
     formatOutputChange();
-  }, [changeArr, outputCost]);
+  }, [changeArr]);
 
   console.log('in: ' + timeIn)
   console.log('out: ' + timeOut)
@@ -32,73 +88,6 @@ function App() {
   console.log('paymentArray: ' + paymentArray)
   console.log('changeArr: ' + changeArr)
   console.log('outputChange: ' + outputChange)
-
-  function calculateCost() {
-    let timeInArr = timeIn.split(':');
-    let timeOutArr = timeOut.split(':');
-    let numOfMinsAtEntry = Number((timeInArr[0] * 60)) + Number(timeInArr[1])
-    let numOfMinsAtExit = Number((timeOutArr[0] * 60)) + Number(timeOutArr[1])
-    //if exit time is the following day, add 24 hours to timeout
-    if (Number(timeInArr[0]) <= Number(timeOutArr[0])) {
-      let numOfMinsInCarPark = numOfMinsAtExit - numOfMinsAtEntry;
-      if(numOfMinsInCarPark > 60) {
-        setOutputCost(3 + (numOfMinsInCarPark - 60) / 100);
-      } else if (numOfMinsInCarPark <= 60) {
-        setOutputCost(3);
-      }
-    } else {
-      let numOfMinsInCarPark = (numOfMinsAtExit + (24 * 60)) - numOfMinsAtEntry;
-      setOutputCost(3 + ((numOfMinsInCarPark - 60) / 100));
-    }
-  }
-
-  function processPaymentInput() {
-    let inputArray = paymentIn.split(', ');
-    let processedInputArray = [];
-    for (let element of inputArray) {
-      if (element[0] === '£') {
-        processedInputArray.push(Number(element.slice(1)));
-      } else {
-        processedInputArray.push(element.match(/\d+/g) / 100);
-      }
-    }
-    setPaymentArray(processedInputArray);
-  }
-
-  function calculateMinChange() {
-    //calculate sum of payment in
-    let paymentSum = paymentArray.reduce((a, b) => a + b, 0);
-    let change = (paymentSum - outputCost).toFixed(2)
-    let coins = [0.01, 0.02, 0.05, 0.10, 0.20, 0.50, 1, 5, 10, 20]
-    let descCoins = coins.sort((a,b) => b-a);
-    let changeArray = []
-    for (let i = 0; i < descCoins.length; i++) {
-      while (descCoins[i] <= change ) {
-        changeArray.push(descCoins[i]);
-        change = (change - descCoins[i]).toFixed(2)
-      }
-    }
-    setChangeArr(changeArray);
-  }
-
-  function formatOutputChange() {
-    let formattedArray = []
-    for (let element of changeArr) {
-      if (element < 1) {
-        formattedArray.push(element * 100 + 'p')
-      } else {
-        formattedArray.push('£' + element)
-      }
-    }
-    setOutputChange(formattedArray.join(', '));
-  }
-
-  // function processCarParkingCharge(timeIn, timeOut, paymentIn, outputCost, paymentArray, changeArr) {
-  //   calculateCost(timeIn, timeOut);
-  //   processPaymentInput(paymentIn);
-  //   calculateMinChange(outputCost, paymentArray);
-  //   formatOutputChange(changeArr);
-  // }
 
   return (
     <div className="App">
--- src/components/OutputCost.js
+++ src/components/OutputCost.js
@@ -2,21 +2,7 @@ import React, {useContext, useEffect} from 'react';
 import {AppContext} from '../appContext';
 
 const OutputCost = () => {
-  const { timeIn, timeOut, calculateCost, outputCost  } = useContext(AppContext);
 
-  useEffect(() => {
-    calculateCost();
-  }, [calculateCost]);
-
-
-  console.log('cost :' + outputCost)
-
-  return (
-    <div>
-      <p>Output cost: </p>
-       £{outputCost || outputCost === 0 ? outputCost.toFixed(2) : '0.00'}
-    </div>
-  )
 }
 
 export default OutputCost;
